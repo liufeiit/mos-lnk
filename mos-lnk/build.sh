f8a5f8a212ec11756b50b56d5f148ea64b615001
liufei_it <liufei_it@126.com>
2015-06-05 10:38:52 +0800

    install lnk

--- mos-lnk/build.sh
+++ mos-lnk/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 mvn clean install -Dmaven.test.skip=true
-echo "Build Mos-Lnk Server Success."
+echo "Build Mos-Lnk Server Success ."
+echo "Install Lnk Server... "
+echo "copy build/lib/*.jar lnk/lib/" && cp build/lib/*.jar lnk/lib/
+echo "copy etc/*.xml lnk/etc/" && cp etc/*.xml lnk/etc/
+echo "copy etc/*.sh lnk/bin/" && cp etc/*.sh lnk/bin/
+echo "copy target/mos-lnk.jar lnk/bin/" && cp target/mos-lnk.jar lnk/bin/
+echo "Install Lnk Server Success."
